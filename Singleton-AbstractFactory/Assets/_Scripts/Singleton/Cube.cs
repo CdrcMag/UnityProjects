601a0e8d05074c7a2a0c545fcd365deef0238144
CdrcMag <cdrcmag@outlook.fr>
2020-11-03 11:31:49 +0100

    Fin singleton

---
+++ Singleton-AbstractFactory/Assets/_Scripts/Singleton/Cube.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+
+    private void Start()
+    {
+        MeshRenderer renderer = GetComponent<MeshRenderer>();
+
+        if(renderer)
+        {
+            if (GameState.Instance.isRunning)
+                renderer.material.color = Color.green;
+            else
+                renderer.material.color = Color.red;
+        }
+        
+    }
+
+}
