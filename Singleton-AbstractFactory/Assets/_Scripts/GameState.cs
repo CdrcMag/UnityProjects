7fdc34da5e82c3113536bfa106b728f45a75b211
CdrcMag <cdrcmag@outlook.fr>
2020-11-03 10:49:37 +0100

    Ajout du singleton

---
+++ Singleton-AbstractFactory/Assets/_Scripts/GameState.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour
+{
+    //Instance du singleton
+    public static GameState Instance;
+
+    //Variables contenue dans le singleton
+    public bool isRunning = true;
+
+    //Setup
+    private void Awake()
+    {
+        Instance = this;
+        if (Instance == null)
+        {
+            Instance = GameObject.FindObjectOfType<GameState>();
+            if (Instance == null)
+            {
+                GameObject container = new GameObject("GameState");
+                Instance = container.AddComponent<GameState>();
+            }
+        }
+    }
+
+
+    
+}
